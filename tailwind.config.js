9940f9ac4b916dbecaf9fc1b0978625c3f467050
webDOTsummon <alice.alfvin@gmail.com>
2026-04-20 14:47:26 +0200

    added tailwind config and primary color palette

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#F9CCC8',
+          100: '#F5958A',
+          200: '#ED5735',
+          300: '#B13F25',
+          400: '#792816',
+          500: '#451408',
+          600: '#250703'
+        },
+      }
+    },
+  },
+}
