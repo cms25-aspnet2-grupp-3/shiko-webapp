86e83600b7de26927dfe49d8e5b598ddc9c0a84b
Oscar Taberman <oscar.taberman@yh.nackademin.se>
2026-04-21 15:45:22 +0200

    feat: started implementing search bar in header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,31 @@
 import styles from "./Header.module.css";
+import Image from "next/image";
+import searchIcon from "@/assets/Icons/search-icon.png";
 
 export default function Header() {
     return (
         <header className={styles.header}>
-                <div className={styles["search-bar"]}></div>
+            <form className={styles["search-bar"]}>
+                <button type="submit">
+                    <Image src={searchIcon} alt="Search" width={40} height={40} />
+                </button>
+                <input type="text" placeholder="Search..." />
+            </form>
 
-                <div className={styles["right-section"]}>
-                    <button className={styles.button}></button>
-                    <div className={styles.divider}></div>
-                    <button className={styles.button}></button>
-                    <div className={styles.divider}></div>
+            <div className={styles["right-section"]}>
+                <button className={styles.button}></button>
+                <div className={styles.divider}></div>
+                <button className={styles.button}></button>
+                <div className={styles.divider}></div>
 
-                    <div className={styles["user-profile"]}>
-                        <span className={styles["user-image"]}></span>
-                        <div className={styles["user-info"]}>
-                            <span className={styles["user-name"]}>Oscar Taberman</span>
-                            <span className={styles["user-email"]}>oscar.taberman@example.com</span>
-                        </div>
+                <div className={styles["user-profile"]}>
+                    <span className={styles["user-image"]}></span>
+                    <div className={styles["user-info"]}>
+                        <span className={styles["user-name"]}>Oscar Taberman</span>
+                        <span className={styles["user-email"]}>oscar.taberman@example.com</span>
                     </div>
                 </div>
+            </div>
         </header>
     );
 }
