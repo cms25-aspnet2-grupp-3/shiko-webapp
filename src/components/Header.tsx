2e87e3ac0c14ad2ebfec99afa37f2cb08d2e2af0
Oscar Taberman <oscar.taberman@yh.nackademin.se>
2026-04-21 13:07:43 +0200

    feat: refactor header component styles and integrate into AppShellLayout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,24 @@
 import styles from "./Header.module.css";
-import Image from "next/image";
 
 export default function Header() {
     return (
         <header className={styles.header}>
+                <div className={styles["search-bar"]}></div>
 
-            <nav className={styles.nav}>
-                <div className={styles["left-section"]}>
-                    <div className={styles["search-bar"]}></div>
-                </div>
                 <div className={styles["right-section"]}>
                     <button className={styles.button}></button>
+                    <div className={styles.divider}></div>
                     <button className={styles.button}></button>
+                    <div className={styles.divider}></div>
+
                     <div className={styles["user-profile"]}>
-                        <span className={styles["user-image"]}></span>{/* <Image src="" alt="User Profile" className="profile-pic" /> */}
+                        <span className={styles["user-image"]}></span>
                         <div className={styles["user-info"]}>
                             <span className={styles["user-name"]}>Oscar Taberman</span>
                             <span className={styles["user-email"]}>oscar.taberman@example.com</span>
                         </div>
                     </div>
                 </div>
-            </nav>
         </header>
     );
 }
