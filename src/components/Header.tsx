b8b74fd8fccba81d94f88f15586adf31861b322e
Oscar Taberman <oscar.taberman@yh.nackademin.se>
2026-04-22 00:54:05 +0200

    feat: update header component with new styles and icons, remove unused images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,31 @@
 import styles from "./Header.module.css";
 import Image from "next/image";
-import searchIcon from "@/assets/Icons/search-icon.png";
+
 
 export default function Header() {
     return (
         <header className={styles.header}>
             <form className={styles["search-bar"]}>
-                <button type="submit">
-                    <Image src={searchIcon} alt="Search" width={40} height={40} />
+                <button type="submit" className={styles["submit"]}>
+                    <Image src="/icons/icon-search.svg" alt="Search" width={16} height={16} />
                 </button>
                 <input type="text" placeholder="Search..." />
             </form>
 
             <div className={styles["right-section"]}>
-                <button className={styles.button}></button>
+                <button className={styles.button}>
+                    <Image src="/icons/icon.svg" alt="Mail" width={21.18} height={18} />
+                </button>
                 <div className={styles.divider}></div>
-                <button className={styles.button}></button>
+                <button className={styles.button}>
+                    <Image src="/icons/notification.svg" alt="Notification" width={15.3} height={18} />
+                </button>
                 <div className={styles.divider}></div>
 
                 <div className={styles["user-profile"]}>
-                    <span className={styles["user-image"]}></span>
+                    <button className={styles["user-image"]}>
+                        <Image src="/images/profile-sample.png" alt="Notification" width={60} height={60} />
+                    </button>
                     <div className={styles["user-info"]}>
                         <span className={styles["user-name"]}>Oscar Taberman</span>
                         <span className={styles["user-email"]}>oscar.taberman@example.com</span>
