1fd09f9a5242482b8d9299bac9e72db46fb89736
webDOTsummon <alice.alfvin@gmail.com>
2026-04-22 15:11:12 +0200

    created Course Information css

--- src/components/CourseInformationBox.tsx
+++ src/components/CourseInformationBox.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import styles from "./CourseInformation.module.css";
+import Image from "next/image";
 
-const CourseInformation = () => {
+const TotalCourseInfo = () => {
 return (
-    <div>
+    <div className={styles["totalcourse"]}>
+        <div className="upper1">
+            <Image src="/icons/Open Book.svg" alt="Open Book icon" width={20} height={20} />
+        </div>
+        <h3>Total Course</h3>
+    </div>
+)}
 
+const CourseCompletedInfo = () => {
+return (
+    <div className={styles["coursecompleted"]}>
+        <div className="upper2">
+            <Image src="/icons/Paper.svg" alt="Paper icon" width={20} height={20} />
+        </div>
+        <h3>Course Completed</h3>
+    </div>
+)}
+
+const CourseInProgressInfo = () => {
+return (
+    <div className={styles["courseinprogress"]}>
+        <div className="upper3">
+            <Image src="/icons/icon-courseinprogress.svg" alt="In Progress icon" width={20} height={20} />
+        </div>
+        <h3>Course In Progress</h3>
+    </div>
+)}
+
+const CompletedLiveCoursesInfo = () => {
+return (
+    <div className={styles["completedlivecourses"]}>
+        <div className="upper4">
+            <Image src="/icons/Group 1.svg" alt="Play Button icon" width={20} height={20} />
+        </div>
+        <h3>Upcoming Live Courses</h3>
     </div>
 )}
