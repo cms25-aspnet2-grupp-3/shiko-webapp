3f0ecbfb77ac3ec101b3c2d592811d16ba970b66
webDOTsummon <alice.alfvin@gmail.com>
2026-04-23 21:54:22 +0200

    page.tsx now imports CourseInformationBox

--- src/app/(public)/dashboard/page.tsx
+++ src/app/(public)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 
 import ChatBox from "@/components/ChatBox";
+import CourseInformationBox from "@/components/CourseInformationBox";
 
 import HeroContent from "@/features/hero-content/hero-content";
 import HeroContentSkeleton from "@/features/hero-content/hero-content-skeleton";
@@ -24,40 +25,8 @@ export default function Home({
 
   return (
     <div className={`grid ${gapClassName} ${roundedClassName}`}>
-      <div
-        className={`grid grid-cols-1 ${gapClassName} sm:grid-cols-2 3xl:grid-cols-4 3xl:grid-rows-2`}
-      >
-        <section
-          className={`${cardBaseClassName} 3xl:col-span-1 3xl:row-start-1`}
-        >
-          Total Course
-        </section>
-
-        <section
-          className={`${cardBaseClassName} 3xl:col-span-1 3xl:row-start-1`}
-        >
-          Course Complete
-        </section>
-
-        <section
-          className={`${cardBaseClassName} 3xl:col-span-1 3xl:row-start-2`}
-        >
-          Course In Progress
-        </section>
-
-        <section
-          className={`${cardBaseClassName} 3xl:col-span-1 3xl:row-start-2`}
-        >
-          Upcoming Live Courses
-        </section>
-
-        <section
-          className={`${cardRoundedClassName} ${cardBackgroundClassName} p-8 pb-0 sm:col-span-2 3xl:col-span-2 3xl:row-span-2 3xl:h-full overflow-hidden`}
-        >
-          <Suspense fallback={<HeroContentSkeleton />}>
-            <HeroContent />
-          </Suspense>
-        </section>
+      <div>
+        <CourseInformationBox />
       </div>
 
       <div
--- src/components/CourseInformationBox.tsx
+++ src/components/CourseInformationBox.tsx
@@ -1,42 +1,38 @@
 import styles from "./CourseInformation.module.css";
 import Image from "next/image";
 
-const TotalCourseInfo = () => {
+const CourseInformationBox = () => {
 return (
-    <div className={styles["totalcourse"]}>
-        <div className="upper1">
-            <Image src="/icons/Open Book.svg" alt="Open Book icon" width={20} height={20} />
+    <div>
+        <div className={styles["totalcourse"]}>
+            <div className="upper1">
+                <Image src="/icons/Open Book.svg" alt="Open Book icon" width={20} height={20} />
+            </div>
+            <h1>Total Course</h1>
         </div>
-        <h3>Total Course</h3>
-    </div>
-)}
 
-const CourseCompletedInfo = () => {
-return (
-    <div className={styles["coursecompleted"]}>
-        <div className="upper2">
-            <Image src="/icons/Paper.svg" alt="Paper icon" width={20} height={20} />
+        <div className={styles["coursecompleted"]}>
+            <div className="upper2">
+                <Image src="/icons/Paper.svg" alt="Paper icon" width={20} height={20} />
+            </div>
+            <h3>Course Completed</h3>
         </div>
-        <h3>Course Completed</h3>
-    </div>
-)}
 
-const CourseInProgressInfo = () => {
-return (
-    <div className={styles["courseinprogress"]}>
-        <div className="upper3">
-            <Image src="/icons/icon-courseinprogress.svg" alt="In Progress icon" width={20} height={20} />
+        <div className={styles["courseinprogress"]}>
+            <div className="upper3">
+                <Image src="/icons/icon-courseinprogress.svg" alt="In Progress icon" width={20} height={20} />
+            </div>
+            <h3>Course In Progress</h3>
         </div>
-        <h3>Course In Progress</h3>
-    </div>
-)}
 
-const CompletedLiveCoursesInfo = () => {
-return (
-    <div className={styles["completedlivecourses"]}>
-        <div className="upper4">
-            <Image src="/icons/Group 1.svg" alt="Play Button icon" width={20} height={20} />
+        <div className={styles["completedlivecourses"]}>
+            <div className="upper4">
+                <Image src="/icons/Group 1.svg" alt="Play Button icon" width={20} height={20} />
+            </div>
+            <h3>Upcoming Live Courses</h3>
         </div>
-        <h3>Upcoming Live Courses</h3>
     </div>
-)}
+    );
+};
+
+export default CourseInformationBox
