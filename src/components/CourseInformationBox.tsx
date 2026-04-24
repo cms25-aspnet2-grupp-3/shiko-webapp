cc72c9ea479e4199f8558cb116e7999448cc9e5d
webDOTsummon <alice.alfvin@gmail.com>
2026-04-24 11:15:26 +0200

    style: minor adjustments

--- src/components/CourseInformationBox.tsx
+++ src/components/CourseInformationBox.tsx
@@ -3,33 +3,45 @@ import Image from "next/image";
 
 const CourseInformationBox = () => {
 return (
-    <div className="grid grid-cols-2 place-self-start space-x-8" >
+    <div className="grid grid-cols-2 place-self-start gap-8" >
         <section className={styles["totalcourse"]}>
-            <div className="informationicon">
-                <Image src="/icons/Open Book.svg" alt="Open Book icon" width={20} height={20} />
+            <div className="infobox">
+                <div className={styles["informationicon"]}>
+                    <Image src="/icons/Open Book.svg" alt="Open Book icon" width={20} height={20} />
+                </div>
+                <p className="font-bold text-lg">Total Course</p>
+                <a>view details placeholder</a>
             </div>
-            <p>Total Course</p>
         </section>
 
-        <section className={styles["coursecompleted"]}>
-            <div className="informationicon">
-                <Image src="/icons/Paper.svg" alt="Paper icon" width={20} height={20} />
+        <section className={styles["coursecomplete"]}>
+            <div className="infobox">
+                <div className={styles["informationicon"]}>
+                    <Image src="/icons/Paper.svg" alt="Paper icon" width={20} height={20} />
+                </div>
+                <p className="font-bold text-lg">Course Complete</p>
+                <a>view details placeholder</a>
             </div>
-            <p>Course Completed</p>
         </section>
 
         <section className={styles["courseinprogress"]}>
-            <div className="informationicon">
-                <Image src="/icons/icon-courseinprogress.svg" alt="In Progress icon" width={20} height={20} />
+            <div className="infobox">
+                <div className={styles["informationicon"]}>
+                    <Image src="/icons/icon-courseinprogress.svg" alt="In Progress icon" width={20} height={20} />
+                </div>
+                <p className="font-bold text-lg">Course In Progress</p>
+                <a>view details placeholder</a>
             </div>
-            <p>Course In Progress</p>
         </section>
 
         <section className={styles["completedlivecourses"]}>
-            <div className="informationicon">
-                <Image src="/icons/Group 1.svg" alt="Play Button icon" width={20} height={20} />
+            <div className="infobox">
+                <div className={styles["informationicon"]}>
+                    <Image src="/icons/Group 1.svg" alt="Play Button icon" width={20} height={20} />
+                </div>
+                <p className="font-bold text-lg">Upcoming Live Courses</p>
+                <a>view details placeholder</a>
             </div>
-            <p>Upcoming Live Courses</p>
         </section>
     </div>
     );
