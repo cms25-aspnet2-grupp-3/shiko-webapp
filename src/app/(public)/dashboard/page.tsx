a5b4e3a70465147568657605d5353cb6d97c688d
ihsnack <ihsnack@fastmail.com>
2026-04-26 10:34:32 +0200

    feat: display hero content api data in course box

--- src/app/(public)/dashboard/page.tsx
+++ src/app/(public)/dashboard/page.tsx
@@ -1,4 +1,9 @@
+import { Suspense } from "react";
+
 import ChatBox from "@/components/ChatBox";
+
+import CurrentHeroContentView from "@/features/hero-content/current-hero-content-view";
+import CurrentHeroContentSkeleton from "@/features/hero-content/current-hero-content-skeleton";
 
 type HomeProps = Readonly<{
   gapClassName?: string;
@@ -22,22 +27,36 @@ export default function Home({
       <div
         className={`grid grid-cols-1 ${gapClassName} sm:grid-cols-2 3xl:grid-cols-4 3xl:grid-rows-2`}
       >
-        <section className={`${cardBaseClassName} 3xl:col-span-1 3xl:row-start-1`}>
+        <section
+          className={`${cardBaseClassName} 3xl:col-span-1 3xl:row-start-1`}
+        >
           Total Course
         </section>
-        <section className={`${cardBaseClassName} 3xl:col-span-1 3xl:row-start-1`}>
+
+        <section
+          className={`${cardBaseClassName} 3xl:col-span-1 3xl:row-start-1`}
+        >
           Course Complete
         </section>
-        <section className={`${cardBaseClassName} 3xl:col-span-1 3xl:row-start-2`}>
+
+        <section
+          className={`${cardBaseClassName} 3xl:col-span-1 3xl:row-start-2`}
+        >
           Course In Progress
         </section>
-        <section className={`${cardBaseClassName} 3xl:col-span-1 3xl:row-start-2`}>
+
+        <section
+          className={`${cardBaseClassName} 3xl:col-span-1 3xl:row-start-2`}
+        >
           Upcoming Live Courses
         </section>
+
         <section
           className={`${cardBaseClassName} sm:col-span-2 3xl:col-span-2 3xl:row-span-2 3xl:h-full`}
         >
-          Course
+          <Suspense fallback={<CurrentHeroContentSkeleton />}>
+            <CurrentHeroContentView />
+          </Suspense>
         </section>
       </div>
 
@@ -50,7 +69,9 @@ export default function Home({
           Your assignments
         </section>
 
-        <div className={`${cardBaseClassName} lg:col-start-4 lg:col-span-1 lg:row-start-1`}>
+        <div
+          className={`${cardBaseClassName} lg:col-start-4 lg:col-span-1 lg:row-start-1`}
+        >
           <ChatBox />
         </div>
 
