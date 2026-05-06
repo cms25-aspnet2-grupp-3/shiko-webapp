29d0122dd75146f6b33500c00024c2316e710881
ihsnack <ihsnack@fastmail.com>
2026-05-06 14:25:09 +0200

    fix: temporary redirect from / to /dashboard

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
